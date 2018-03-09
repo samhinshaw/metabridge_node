c69a690a17a4a8c0a1a758b35504c42a20b4cd2e
Sam Hinshaw <samuel.hinshaw@gmail.com>
2018-03-08 17:09:05 -0800

    Working on upload page

--- pages/upload.jsx
+++ pages/upload.jsx
@@ -14,34 +14,66 @@ const MainPanel = glamorous.div({
   // backgroundColor: '#38A2FF'
 });
 
+const Instructions = glamorous.div({
+  marginBottom: '1.5rem'
+});
+
+const RadioLabel = glamorous.span({
+  marginLeft: '5px'
+});
+
 const Upload = () => (
   <Layout>
     <div className="section">
       <div className="tile is-ancestor">
         <UploadPanel className="tile is-parent is-vertical is-3">
-          <div className="tile is-child">
-            <h2 className="title is-size-3">Upload</h2>
-            <div className="box">
-              <p>
+          <div className="card">
+            <div className="card-content">
+              <h2 className="title is-size-3">Upload</h2>
+              <Instructions>
                 Upload a plain-text spreadsheet (CSV or TSV) containing your metabolites of interest
                 in a single column, or try out our example dataset.
-              </p>
-              <div className="file">
-                <label className="file-label" htmlFor="metaboliteUpload">
-                  <input
-                    className="file-input"
-                    type="file"
-                    name="metaboliteUpload"
-                    id="metaboliteUpload"
-                  />
-                  <span className="file-cta">
-                    <span className="file-icon">
-                      <i className="fas fa-upload" />
+              </Instructions>
+              <form action="">
+                <div className="field">
+                  <label className="file-label" htmlFor="metaboliteUpload">
+                    <strong>Upload Metabolites</strong>
+                  </label>
+                  <div className="file" id="fileUpload">
+                    <input
+                      className="file-input"
+                      type="file"
+                      name="metaboliteUpload"
+                      id="metaboliteUpload"
+                    />
+                    <span className="file-cta">
+                      <span className="file-icon">
+                        <i className="fas fa-upload" />
+                      </span>
+                      <span className="file-label">Choose a file...</span>
                     </span>
-                    <span className="file-label">Choose a file…</span>
-                  </span>
-                </label>
-              </div>
+                  </div>
+                </div>
+                <div className="field">
+                  <label htmlFor="separator">
+                    <strong>Separator</strong>
+                  </label>
+                  <div className="control" id="separator">
+                    <label className="radio" htmlFor="commaSep">
+                      <input type="radio" name="separator" id="commaSep" />
+                      <RadioLabel>Comma</RadioLabel>
+                    </label>
+                    <label className="radio" htmlFor="tabSep">
+                      <input type="radio" name="separator" id="tabSep" />
+                      <RadioLabel>Tab</RadioLabel>
+                    </label>
+                    <label className="radio" htmlFor="semiSep">
+                      <input type="radio" name="separator" id="semiSep" />
+                      <RadioLabel>Semicolon</RadioLabel>
+                    </label>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
         </UploadPanel>
