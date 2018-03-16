7f607716b35486f2eff2eaf69a5a5bf85ed5770d
Sam Hinshaw <samuel.hinshaw@gmail.com>
2018-03-15 20:03:48 -0700

    Use fast-csv to read in csv files!!

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const Rollbar = require('rollbar');
 // const bodyParser = require('body-parser');
 // const expressSanitizer = require('express-sanitizer');
 const multiparty = require('multiparty');
+// const fs = require('fs');
+const csv = require('fast-csv');
 
 const rollbar = new Rollbar('ceb9067b85ae4c5a91c08b617d9f94c3');
 const dev = process.env.NODE_ENV !== 'production';
@@ -20,12 +22,24 @@ app
     // server.use(bodyParser.urlencoded({ extended: true }));
     // server.use(expressSanitizer()); // this line follows bodyParser() instantiations
 
+    // Think about bruteforce prevention here (to stop our server from getting hammered)
     server.post('/upload', (req, res) => {
       const form = new multiparty.Form();
-      form.parse(req, (err, fields, files) => {
-        console.log('errors: ', err);
-        console.log('fields: ', fields);
-        console.log('files: ', files);
+      form.parse(req, async (err, fields, files) => {
+        // We should really just be getting only one file
+        if (files.file.length > 1) {
+          res.status(200).json({ message: 'Please only upload one file' });
+        } else {
+          csv
+            .fromPath(files.file[0].path)
+            .on('data', data => {
+              console.log(data);
+            })
+            .on('end', () => {
+              console.log('done');
+            });
+        }
+        res.status(200).json({});
         // res.writeHead(200, { 'content-type': 'text/plain' });
         // res.write('received upload:\n\n');
         // res.end(util.inspect({ fields, files }));
