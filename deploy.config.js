39663ae683badd11e7d57ef888d427e10d9251a4
Sam Hinshaw <samuel.hinshaw@gmail.com>
2018-03-19 11:39:36 -0700

    Add PM2 deploy configuration

---
+++ deploy.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  // Application configuration section
+  // http://pm2.keymetrics.io/docs/usage/application-declaration/
+
+  apps: [
+    // First application
+    {
+      name: 'metabridge',
+      script: 'npm',
+      args: 'run start',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  // Deployment section
+  // http://pm2.keymetrics.io/docs/usage/deployment/
+
+  deploy: {
+    production: {
+      user: 'sam',
+      host: 'metabridge.org',
+      ref: 'origin/master',
+      repo: 'git@github.com:samhinshaw/metabridge_node.git',
+      path: '/home/sam/node_apps/metabridge',
+      ssh_options: 'ForwardAgent=yes',
+      'post-deploy':
+        'yarn install && yarn build && pm2 startOrRestart deploy.config.js --env production && yarn rollbar',
+      env: {
+        PATH:
+          '/home/sam/.nvm/versions/node/v8.10.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games'
+      }
+    }
+    // dev: {
+    //   user: 'node',
+    //   host: '212.83.163.1',
+    //   ref: 'origin/master',
+    //   repo: 'git@github.com:repo.git',
+    //   path: '/var/www/development',
+    //   'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env dev',
+    //   env: {
+    //     NODE_ENV: 'dev'
+    //   }
+    // }
+  }
+};
