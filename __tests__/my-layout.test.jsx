26e30a57b0603dd195b5497c62608d3202b860fe
Sam Hinshaw <samuel.hinshaw@gmail.com>
2018-09-03 17:11:54 -0700

    Update Jest tests

--- __tests__/my-layout.test.jsx
+++ __tests__/my-layout.test.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { shallow } from 'enzyme';
 import Layout from '../components/my-layout';
 import mockChildren from '../__mocks__/children';
 
+// Currently running into issues testing this. It does not affect the other test,
+// so it most certainly is related to the mock children.
+
+const TestLayout = () => (
+  <Fragment>
+    <Layout>{mockChildren}</Layout>
+  </Fragment>
+);
+
 test('Search renders correctly', () => {
-  const component = shallow(<Layout>{mockChildren}</Layout>);
+  const component = shallow(<TestLayout />);
   expect(component).toMatchSnapshot();
 });
--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,8 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/file-mock.js',
     // '\\.(css|less)$': 'identity-obj-proxy' // also works
-    '\\.(css|less)$': '<rootDir>/__mocks__/style-mock.js'
-  }
+    // '\\.(css|less)$': '<rootDir>/__mocks__/style-mock.js',
+    '\\.(css|less|scss)$': 'identity-obj-proxy' // also works
+  },
+  testURL: 'http://localhost/'
 };
